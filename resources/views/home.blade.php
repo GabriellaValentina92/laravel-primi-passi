615d0e0fb86766fa2a3f8b89dbf32941eb2e7fe2
GabriellaValentina92 <gabriella.valentina@outlook.it>
2023-06-22 16:21:16 +0200

    add hello world

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <style>
+            *{
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <h1 style="text-align: center; font-size: 50px; color: white; background: linear-gradient(to right, red,orange,yellow,green,blue,indigo,violet); text-shadow: 4px 4px; padding: 0.6rem;">HELLO WORLD</h1>
+        </div>
+    </body>
+</html>
